f3a01770b0bbf520da2269622dda0b9ea3ec52f4
Manuel Orlando <manuelorlandokebortiz@gmail.com>
2025-04-23 08:08:32 -0600

    screens creadas y barril actualizado

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,4 +1,15 @@
+
+
+//ctrl+} es para comentar una linea
+//ctrl+d es para buscar una variable revisar todas
+//ctrl+shift+alt para comentar /**/ se selecciona primero que 
+//ctrl+shift+l para un mismo variable
+export 'package:widgets_app/presentation/screens/animated/animated_screen.dart';
+export 'package:widgets_app/presentation/screens/app-tutoriaal/app_tutorial_screen.dart';
 export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_app/presentation/screens/home/home_screen.dart';
-
+export 'package:widgets_app/presentation/screens/infinite_scroo/infinite_scroll_screen.dart';
+export 'package:widgets_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widgets_app/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/presentation/screens/ui_controls/ui_controls_screen.dart';
