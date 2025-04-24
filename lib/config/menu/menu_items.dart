e2e0c1eece1d590db9455925378289be956be319
Manuel Orlando <manuelorlandokebortiz@gmail.com>
2025-04-24 11:48:06 -0600

    animated container terminado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -1,41 +1,47 @@
 import 'package:flutter/material.dart';
+
 class MenuItem {
   final String title;
   final String subTitle;
   final String link;
   final IconData icon;
 
-  const MenuItem({
-    required this.title, 
-    required this.subTitle, 
-    required this.link, 
-    required this.icon
-    });
+  const MenuItem(
+      {required this.title,
+      required this.subTitle,
+      required this.link,
+      required this.icon});
 }
 
 const appMenuItems = <MenuItem>[
-    MenuItem(
-      title: 'Botones',
-      subTitle: 'Varios Botones En Flutter',
-      link: '/buttons',
-      icon: Icons.smart_button_outlined,
-    ),
-    MenuItem(
-      title: 'Tarjetas',
-      subTitle: 'Un contenedor estilizado',
-      link: '/cards',
-      icon: Icons.credit_card,
-    ),
-    MenuItem(
-      title: 'Progress Indicators',
-      subTitle: 'Generales y controlados',
-      link: '/progress',
-      icon: Icons.refresh_rounded,
-    ),
-    MenuItem(
-      title: 'Snackbar y dialogues',
-      subTitle: 'Indicadores en pantalla',
-      link: '/snackbar',
-      icon: Icons.info_outline,
-    ),
-  ];
+  MenuItem(
+    title: 'Botones',
+    subTitle: 'Varios Botones En Flutter',
+    link: '/buttons',
+    icon: Icons.smart_button_outlined,
+  ),
+  MenuItem(
+    title: 'Tarjetas',
+    subTitle: 'Un contenedor estilizado',
+    link: '/cards',
+    icon: Icons.credit_card,
+  ),
+  MenuItem(
+    title: 'Progress Indicators',
+    subTitle: 'Generales y controlados',
+    link: '/progress',
+    icon: Icons.refresh_rounded,
+  ),
+  MenuItem(
+    title: 'Snackbar y dialogues',
+    subTitle: 'Indicadores en pantalla',
+    link: '/snackbar',
+    icon: Icons.info_outline,
+  ),
+  MenuItem(
+    title: 'Animated Container',
+    subTitle: 'Stateful Widgets animado',
+    link: '/animated',
+    icon: Icons.check_box_outline_blank_rounded,
+  ),
+];
